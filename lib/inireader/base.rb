8d713c76e9a54f347704a2a6c2bbe2a08cebb586
Joel Jackson <jackson.joel@gmail.com>
2013-01-08 17:26:41 -0800

    First failing spec. Gets section name.

--- lib/inireader/base.rb
+++ lib/inireader/base.rb
@@ -1,5 +1,14 @@
 module IniReader
   class Base
+    def initialize( string = '')
+      @section_hash = {}
+    end
+
+    #returns the hash for a section of the inifile
+    def []( section_name )
+      return @section_hash[section_name]
+    end
+
     
   end
 end
--- spec/inireader/base_spec.rb
+++ spec/inireader/base_spec.rb
@@ -4,4 +4,15 @@ describe IniReader::Base do
       reader = IniReader::Base.new
     end.should_not raise_error
   end
+
+  it "should return nil if looking for a section that doesn't exist" do
+    reader = IniReader::Base.new
+    reader[:section_name].should == nil
+  end
+
+  it "should add a section when it sees a section header" do
+    section_string = "[section_name]\n"
+    reader = IniReader::Base.new(section_string)
+    reader[:section_name].should_not == nil
+  end
 end
