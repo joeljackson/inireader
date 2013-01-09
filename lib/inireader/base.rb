df830a635282ddd086f9485830ae5c31dbb035a7
Joel Jackson <jackson.joel@gmail.com>
2013-01-08 17:33:02 -0800

    basic header spec passes

--- lib/inireader/base.rb
+++ lib/inireader/base.rb
@@ -2,6 +2,7 @@ module IniReader
   class Base
     def initialize( string = '')
       @section_hash = {}
+      parse(string)
     end
 
     #returns the hash for a section of the inifile
@@ -9,6 +10,13 @@ module IniReader
       return @section_hash[section_name]
     end
 
-    
+    private
+    def parse(string)
+      io = StringIO.new(string)
+      io.each do |line|
+        @section_hash[line.match(/\[(.*)\]/)[1].to_sym] = {}
+      end
+    end
+
   end
 end
